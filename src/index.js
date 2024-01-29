9195b49dca99a40ff012bcb699b2d0f1b5e9c437
huyuxing <huyuxing.2022@mapgis.com>
2024-01-29 18:20:51 +0800

    feat/0122:主题包脚手架支持运行时加载 详细描述:主题包脚手架支持运行时加载

--- src/index.js
+++ src/index.js
@@ -10,16 +10,7 @@ const install = (Vue, opts = {}) => {
     if (registerName in Vue.options.components) {
       console.log(`发现同名组件[${registerName}],已取消该组件的注册`)
     } else {
-      Vue.component(registerName, com)
-    }
-  }
-
-  if (opts.components) {
-    for (const key in opts.components) {
-      if (Object.hasOwnProperty.call(opts.components, key)) {
-        const component = opts.components[key]
-        Vue.component(key, component)
-      }
+      Vue.use(com)
     }
   }
 }
